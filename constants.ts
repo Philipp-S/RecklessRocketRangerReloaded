a725839a9d0e57eec1a8ce798196bd0d40c1611e
Philipp Sehmisch <philipp@crushnet.org>
2020-09-19 12:18:09 +0200

    Added player controls

--- constants.ts
+++ constants.ts
@@ -5,6 +5,12 @@ const CONST = {
     GRAVITY: 1000,  // Gravity in pixels per second²
     SCREEN_WIDTH: 1024, 
     SCREEN_HEIGHT: 768,
-    EXPLOSION_FORCE: 100000,
-    EXPLOSION_MIN_RADIUS: 100 //radius from within the maximum explosion force is applied
+    EXPLOSION_FORCE: 10000000,
+    EXPLOSION_MIN_RADIUS: 50, //radius in pixels from within the maximum explosion force is applied
+    EXPLOSION_MAX_RADIUS: 500, //radius in pixels from without explosions are completely ignored
+    PLAYER_ACCEL_GROUND: 2000, // player acceleration on ground
+    PLAYER_FRICTION_GROUND: 0.9, // fraction of speed the player loses per second while grounded
+    PLAYER_FRICTION_AIR: 0.05, // fraction of speed the player loses per second while airborne
+    PLAYER_ACCEL_AIR: 1000, // player acceleration while airborne
+    PLAYER_GROUND_BOUNCE: 0.5 // how much the player bounces when impacting the ground
 }
--- player.ts
+++ player.ts
@@ -1,53 +1,81 @@
 class Player extends Entity {
-    pos: Point;
-    sprite: CanvasImageSource;
-    renderPivot: Point = {x:32, y:64};
+    pos: Point
+    sprite: CanvasImageSource
+    renderPivot: Point = {x:32, y:64}
 
-    private isGrounded = false;
+    private isGrounded = false
 
-    private velocity: Point = {x: 0, y:0};
+    private velocity: Point = {x: 0, y:0}
 
     constructor() {
-        super( { x: 0, y: -500} );
-        let image = new Image();
-        Resources.setImage(this, ImageResource.PLAYER);
+        super( { x: 0, y: -500} )
+        let image = new Image()
+        Resources.setImage(this, ImageResource.PLAYER)
     }
 
     update(deltaTime: number, state: State) : void {
         // apply gravity
-        this.velocity.y += CONST.GRAVITY * deltaTime;
+        this.velocity.y += CONST.GRAVITY * deltaTime
 
+        // shooting
         if(Input.mouse.click) {
-            this.explosion({x: 0, y: 10});
+            this.explosion({x: 0, y: 10})
         }
 
+        // apply input
+        let controlForce = deltaTime * ( this.isGrounded ? CONST.PLAYER_ACCEL_GROUND : CONST.PLAYER_ACCEL_AIR )
+        if(Input.keyDown("KeyA") || Input.keyDown("ArrowLeft")) {
+            this.velocity.x -= controlForce
+        }
+        if(Input.keyDown("KeyD") || Input.keyDown("ArrowRight")) {
+            this.velocity.x += controlForce
+        }
+
+        // apply friction
+        let frictionFraction: number;
+        if (this.isGrounded) {
+            frictionFraction = Math.pow((1 - CONST.PLAYER_FRICTION_GROUND), deltaTime )
+        } else {
+            frictionFraction = Math.pow((1 - CONST.PLAYER_FRICTION_GROUND), deltaTime )
+        }
+        this.velocity.x *= frictionFraction
+        this.velocity.y *= frictionFraction
+
         // apply velocity
-        this.pos.x += this.velocity.x * deltaTime;
-        this.pos.y += this.velocity.y * deltaTime;
+        this.pos.x += this.velocity.x * deltaTime
+        this.pos.y += this.velocity.y * deltaTime
 
         // ground collision checking
         if (this.pos.y > 0) {
             this.pos.y = 0;
-            this.velocity = {x: 0, y: 0}
-            this.isGrounded = true;
+            if (!this.isGrounded) {
+                this.velocity.x *= CONST.PLAYER_GROUND_BOUNCE
+                this.velocity.y *= -CONST.PLAYER_GROUND_BOUNCE
+            }
+            this.isGrounded = true
+        } else {
+            this.isGrounded = false
         }
 
 
     }
 
     explosion(point: Point) {
-        console.log("Explosion");
-        let distx = this.pos.x - point.x;
-        let disty = this.pos.y - point.y;
-        let dist = Math.sqrt(distx * distx + disty * disty);
+        console.log("Explosion")
+        let distx = this.pos.x - point.x
+        let disty = this.pos.y - point.y
+        let dist = Math.sqrt(distx * distx + disty * disty)
 
-        let force = CONST.EXPLOSION_FORCE / ( dist * dist + CONST.EXPLOSION_MIN_RADIUS )
-        let normalized: Point = { x: distx / dist, y: disty / dist };
-
-        // it would be more physically correct to add the explosion force, but
-        // stopping the player actually feels more right
-        this.velocity.x = normalized.x * force;
-        this.velocity.y = normalized.y * force;
+        if (dist <= CONST.EXPLOSION_MAX_RADIUS) {
+            let normalized: Point = { x: distx / dist, y: disty / dist }
+            if (dist < CONST.EXPLOSION_MIN_RADIUS) dist = CONST.EXPLOSION_MIN_RADIUS
+            let force = CONST.EXPLOSION_FORCE / ( dist * dist )
+    
+            // it would be more physically correct to add the explosion force, but
+            // stopping the player actually feels more right
+            this.velocity.x = normalized.x * force
+            this.velocity.y = normalized.y * force
+        }
 
     }
 }
